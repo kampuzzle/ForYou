a5bb0702defe859b92f279344c4bb1ed21de893a
Amanda Apolinario <amandabrito84@gmail.com>
2022-05-02 13:50:15 -0300

    ng model

--- ForYou/src/app/views/login/login.component.ts
+++ ForYou/src/app/views/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -15,8 +16,11 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.user);
   }
 
-
+  public login(): void {
+    console.log(this.user);
+  }
 
 }

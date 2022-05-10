8b0562e013f0653855a5b89c945a8591e9ce29a8
Paula <paulabremenkamp@gmail.com>
2022-05-10 09:48:07 -0300

    versão para testes na API

--- ForYou/src/app/models/category.ts
+++ ForYou/src/app/models/category.ts
@@ -2,10 +2,12 @@ export class Category {
     name: string = '';
     id: string = '';
     icon: any = '';
+    color: any = '';
 
     constructor(category: any) {
         this.name = category.name;
         this.id = category.id;
         this.icon = category.icon;
+        this.color = category.color;
     }
 }
